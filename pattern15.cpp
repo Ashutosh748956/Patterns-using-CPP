5ae5bb41cac8eb889b2a980675c4b3b6791ecd31
Ashutosh748956 <ashuprasad26032002@gmail.com>
2026-09-12 08:52:37 +0530

    pattern15

---
+++ pattern15.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int n = 5;
+for (int i = 0; i < n; i++){
+    
+    
+    for (char  ch = 'A' ; ch < n - i + 'A'; ch++){
+        
+        cout << ch << " ";
+    }
+    cout << endl;
+}
+}
